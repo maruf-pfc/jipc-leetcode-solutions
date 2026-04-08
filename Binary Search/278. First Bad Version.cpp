0f2098b009e1b126bef37ef837c83704954da09c
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:43:33 +0600

    Implement 278. binary search for first bad version

--- Binary Search/278. First Bad Version.cpp
+++ Binary Search/278. First Bad Version.cpp
@@ -1,1 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long low = 1, high = n;
+        long long ans = n;
+
+        while(low <= high) {
+            long long mid = (low + high)/2;
+
+            if(isBadVersion(mid))
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+        return ans;
+    }
+};
