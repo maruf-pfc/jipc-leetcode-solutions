ab7af8e55f39d8cee2e65d781b7395b1b99a2f16
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:59:17 +0600

    Add 1539. solution for Kth Missing Positive Number
    Implement binary search to find the Kth missing positive number.

--- Binary Search/1539. Kth Missing Possitive Number.cpp
+++ Binary Search/1539. Kth Missing Possitive Number.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k) low = mid + 1;
+            else high = mid - 1;
+        }
+        // int missing = arr[high] - (high + 1);
+        // return arr[high] + k - missing;
+        return low + k;
+    }
+};
