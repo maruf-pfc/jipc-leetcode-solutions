6a1713a16a8b457878d978a5def9fe4c82e19840
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-10 22:57:31 +0600

    Update 230. Kth Smallest Value of a BST.cpp

--- Binary Tree/230. Kth Smallest Value of a BST.cpp
+++ Binary Tree/230. Kth Smallest Value of a BST.cpp
@@ -1,1 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inOrder(TreeNode* node, vector<int> &v) {
+        if(!node)return;
 
+        inOrder(node->left, v);
+        v.push_back(node->val);
+        inOrder(node->right, v);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> v;
+        inOrder(root, v);
+        return v[k-1];
+    }
+};
